fabf11a6be66929b82f051d4cb8019175fe506bf
Chmouel Boudjnah <chmouel@chmouel.com>
2024-12-15 19:30:38 +0100

    build with zmk studio because it works well and don't want to mess things up
    ok google
    
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ cPWD="$(dirname $(readlink -f $0))"
 cd $GOPATH/src/gitlab.com/chmouel/zmk/app/ || exit 1
 source $GOPATH/src/gitlab.com/chmouel/zmk/.venv/bin/activate
 
-west build -p -b planck_rev6 -- -DZMK_CONFIG=$cPWD/config -DKEYMAP_FILE=$cPWD/config/planck_rev6.keymap &&
+west build -p -b planck_rev6 -S studio-rpc-usb-uart \
+  -- -DZMK_CONFIG=$cPWD/config -DCONFIG_ZMK_STUDIO=y -DKEYMAP_FILE=$cPWD/config/planck_rev6.keymap &&
   west flash
