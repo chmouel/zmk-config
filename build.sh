ed82824d95cb640dc34f05cf783481d1100d63c2
Chmouel Boudjnah <chmouel@chmouel.com>
2024-12-15 23:15:10 +0100

    improve the build script
    We are now going to wait forever and ever until the keyboard is getting
    plugged and we then wait again to make sure the user is plugging
    properly its keyboard in a way that would be useful for everyone...
    
    We went a bit above and beyond for that detection but we did it the way
    we wanted it to do it, which was damn fine for us.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- build.sh
+++ build.sh
@@ -9,3 +9,26 @@ source $GOPATH/src/gitlab.com/chmouel/zmk/.venv/bin/activate
 west build -p -b planck_rev6 -S studio-rpc-usb-uart \
   -- -DZMK_CONFIG=$cPWD/config -DCONFIG_ZMK_STUDIO=y -DKEYMAP_FILE=$cPWD/config/planck_rev6.keymap &&
   west flash
+
+set +x
+echo -n "Unplug the keyboard: "
+while true; do
+  if dfu-util -l 2>/dev/null | grep -q "Found DFU"; then
+    echo -n "."
+    sleep 1
+  else
+    break
+  fi
+done
+echo " done"
+
+echo -n "Plug the keyboard: "
+while true; do
+  if lsusb | grep -q "Planck"; then
+    break
+  else
+    echo -n "."
+    sleep 1
+  fi
+done
+echo " done"
