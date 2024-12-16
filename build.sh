3a6e0845cd817b5ce52843be0e7233da9c4001a5
Chmouel Boudjnah <chmouel@chmouel.com>
2024-12-16 15:00:28 +0100

    change zmk paths
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 set -euxfo pipefail
 
 cPWD="$(dirname $(readlink -f $0))"
-cd $GOPATH/src/gitlab.com/chmouel/zmk/app/ || exit 1
-source $GOPATH/src/gitlab.com/chmouel/zmk/.venv/bin/activate
+zmkRepo="$(readlink -f $cPWD/../zmk)"
+cd $zmkRepo/app || exit 1
+source $zmkRepo/.venv/bin/activate
 
-west build -p -b planck_rev6 -S studio-rpc-usb-uart \
+west build -b planck_rev6 -S studio-rpc-usb-uart \
   -- -DZMK_CONFIG=$cPWD/config -DCONFIG_ZMK_STUDIO=y -DKEYMAP_FILE=$cPWD/config/planck_rev6.keymap &&
   west flash
 
