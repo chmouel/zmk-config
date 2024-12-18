2e7dd18c2e36d698c327b51de9239a2ee8dde028
Chmouel Boudjnah <chmouel@chmouel.com>
2024-12-18 10:30:15 +0100

    add french keymap and try to fix homerow
    need a lot of work still but we are going to get there hopefully
    
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

---
+++ config/includes/fr.h
@@ -0,0 +1,79 @@
+macros {
+    FR_E_AIGU: FR_E_AIGU {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp SINGLE_QUOTE &kp E>;
+    };
+
+    FR_E_GRAVE: FR_E_GRAVE {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp GRAVE &kp E>;
+    };
+
+    FR_A_GRAVE: FR_A_GRAVE {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp GRAVE &kp A>;
+    };
+
+    FR_U_GRAVE: FR_U_GRAVE {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp GRAVE &kp U>;
+    };
+
+    FR_C_CEDILLE: FR_C_CEDILLE {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp COMMA &kp C>;
+    };
+
+    FR_A_CIRONFL: FR_A_CIRONFL {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp CARET &kp A>;
+    };
+
+    FR_E_CIRONFL: FR_E_CIRONFL {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp CARET &kp E>;
+    };
+
+    FR_I_CIRONFL: FR_I_CIRONFL {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp CARET &kp I>;
+    };
+
+    FR_O_CIRONFL: FR_O_CIRONFL {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp CARET &kp O>;
+    };
+
+    FR_U_CIRONFL: FR_U_CIRONFL {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp CARET &kp U>;
+    };
+
+    FR_E_TREMA: FR_E_TREMA {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp LS(SINGLE_QUOTE) &kp E>;
+    };
+
+    FR_I_TREMA: FR_I_TREMA {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp LS(SINGLE_QUOTE) &kp I>;
+    };
+
+    FR_3_EURO: FR_3_EURO {
+        compatible = "zmk,behavior-macro";
+        #binding-cells = <0>;
+        bindings = <&kp RIGHT_ALT &kp LS(C) &kp EQUAL>;
+    };
+};
