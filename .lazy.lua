efcf4ced6dd34b55cae3dd2226e5a49a777f17a2
Chmouel Boudjnah <chmouel@chmouel.com>
2024-12-22 17:21:04 +0100

    things are working youpi
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- .lazy.lua
+++ .lazy.lua
@@ -7,27 +7,43 @@ return {
 				"<leader>h",
 				"<cmd>TermExec dir="
 					.. vim.fn.system("git rev-parse --show-toplevel"):gsub("\n", "")
-					.. ' cmd="./build.sh"<cr>',
+					.. ' cmd="./%:t:s?.keymap??.sh"<cr>',
 				{ noremap = true, silent = true }
 			)
-			-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-			-- 	pattern = "*.keymap",
-			-- 	callback = function()
-			-- 		require("qmk").format()
-			-- 	end,
-			-- })
-			---@type qmk.UserConfig
-			local conf = {
-				name = "planck",
-				layout = {
-					"x x x x x x x x x x x x",
-					"x x x x x x x x x x x x",
-					"x x x x x x x x x x x x",
-					"x x x x x x x x x x x x",
-				},
-				variant = "zmk",
-			}
-			require("qmk").setup(conf)
+			vim.api.nvim_create_autocmd({ "BufEnter" }, {
+				pattern = "*/planck_rev6.keymap",
+				callback = function()
+					require("qmk").setup({
+						name = "planck",
+						auto_format_pattern = "*/planck_rev6.keymap",
+						layout = {
+							"x x x x x x x x x x x x",
+							"x x x x x x x x x x x x",
+							"x x x x x x x x x x x x",
+							"x x x x x x x x x x x x",
+						},
+						variant = "zmk",
+					})
+					require("qmk").format()
+				end,
+			})
+			vim.api.nvim_create_autocmd({ "BufEnter" }, {
+				pattern = "*/eyelash_corne.keymap",
+				callback = function()
+					require("qmk").setup({
+						name = "eyelash_corne",
+						auto_format_pattern = "*/eyelash_corne.keymap",
+						layout = {
+							"x x x x x x _ _ _ x _ x x x x x x",
+							"x x x x x x _ _ x x x x x x x x x",
+							"x x x x x x _ x _ x _ x x x x x x",
+							"_ _ _ x x x _ _ _ _ _ x x x _ _ _",
+						},
+						variant = "zmk",
+					})
+					require("qmk").format()
+				end,
+			})
 		end,
 	},
 }
