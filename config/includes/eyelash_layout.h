69fd971babc412b62bfed8ab00c15eb5f7adb069
Chmouel Boudjnah <chmouel@chmouel.com>
2024-12-22 19:19:35 +0100

    combos and other stff working now
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

---
+++ config/includes/eyelash_layout.h
@@ -0,0 +1,20 @@
+/*
+    0   1   2   3   4   5        6       7   8   9  10  11 12
+    13  14  15 16  17  18    19  20  21  22  23  24 25  26 27
+    28  29  30 31  32  33  34    35      36  37  38  39 40 41
+               42  43  44                45 46 47
+*/
+
+#define RM1 23
+#define RM2 24
+#define LT5 0
+#define LM4 13
+#define RT5 12
+#define RM4 26
+
+#define KEYS_L                                                                 \
+  0 1 2 3 4 5 13 14 15 16 17 18 28 29 30 31 32 33  // left hand
+#define KEYS_R                                                                 \
+  7 8 9 10 11 12 22 23 24 25 26 27 36 37 38 39 40 41 // right hand
+
+#define THUMBS 42 43 44 45 46 47 // thumbs on 36+ keys
---
+++ config/includes/std.h
@@ -0,0 +1,4 @@
+#define HYPR LS(LC(LA(LGUI)))
+#define QUICK_TAP_MS 175
+#define IDLE_MS 150
+#define TAPPING_TERM_MS 200
